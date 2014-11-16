d1b0472b5726f8e7d2859312fa2736e361b2482e
zhumj13 <zhumj863@nenu.edu.cn>
2014-11-16 10:38:59 +0800

    solution

---
+++ AddBinary/AddBinary/AddBinary.cpp
@@ -0,0 +1,15 @@
+// AddBinary.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+using namespace std;
+string addBinary(string a, string b) {
+        
+}
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+

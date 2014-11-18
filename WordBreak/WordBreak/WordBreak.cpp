b3f335c9e2e230db0f0fa6f102e8f5bd1153446c
zhumj13 <zhumj863@nenu.edu.cn>
2014-11-18 17:07:57 +0800

    wordbreak 修改递归方法 加一个备忘录 这样每次进入函数判断的时候  可以少很多的递归调用 从而省了内存

--- WordBreak/WordBreak/WordBreak.cpp
+++ WordBreak/WordBreak/WordBreak.cpp
@@ -7,19 +7,26 @@
 using namespace std;
 class Solution {
 public:
-	bool wordBreak(string s, unordered_set<string> &dict) {
+	bool wordBreak(string s, unordered_set<string> dict)
+	{
+			unordered_set<string> unmatch;
+			return wordBreak(s,dict,unmatch);
+	}
+	bool wordBreak(string s, unordered_set<string> &dict,unordered_set<string>&unmatch) {
+		if(unmatch.count(s)>0)
+			return false;
 		if(s.length()==0){
 			return true;
 		}
 		for(int i=1;i<=s.length();i++){
-			//string tt =s.substr(0,i);
 			if(dict.count(s.substr(0,i))>0){
 				string sCopy = s.substr(i,s.length()-i);
-				if(wordBreak(sCopy,dict)){
+				if(wordBreak(sCopy,dict,unmatch)){
 					return true;
 				}
 			}
 		}
+		unmatch.insert(s);
 		return false;
 	}
 };
@@ -27,12 +34,20 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	Solution solution;
 	unordered_set<string> dict;
-	dict.insert("leet");
-	dict.insert("code");
-	dict.insert("le");
-	dict.insert("lee");
-	dict.insert("de");
-	string s= "leetcode";
+	//dict.insert("leet");
+	//dict.insert("code");
+	//dict.insert("le");
+	//dict.insert("lee");
+	//dict.insert("de");
+	//string s= "leetcode";
+
+	dict.insert("a");
+	dict.insert("b");
+	dict.insert("abc");
+	dict.insert("cd");
+	string s= "abcd";
+
+	unordered_set<string> unmatch;
 	bool ss=solution.wordBreak(s,dict);
 	return 0;
 }
